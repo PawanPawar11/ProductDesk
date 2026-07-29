349226dc1e576f57776246d7ce3c395fd40e2cea
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-29 20:58:03 +0530

    refactor(auth): replace home page with login flow
    - Remove HomeController and its associated view
    - Add AuthController with Login action
    - Create Login view under Auth
    - Set AuthController.Login as the default route

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductDesk.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
