7575e4e931332809a9ae630c2cadcd5d33914d97
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-30 07:40:59 +0530

    feat(auth): implement user registration workflow
    - Bind register form fields to UserDto
    - Validate user input before creating a new account
    - Register users asynchronously in the database
    - Redirect authenticated users to the Dashboard
    - Display registration errors using ViewBag messages
    - Add Bootstrap Icons CDN for alert icons

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductDesk.Data;
+using ProductDesk.Dto;
+using ProductDesk.Models;
 
 namespace ProductDesk.Controllers
 {
-    public class AuthController : Controller
+    public class AuthController(AppDbContext context) : Controller
     {
         public IActionResult Login()
         {
@@ -12,6 +16,43 @@ namespace ProductDesk.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        public async Task<IActionResult> RegisterUser(UserDto userDto)
+        {
+            if(userDto == null)
+            {
+                ViewBag.Message = "All fields (Username, Email and Password) are required in order to Register!";
+                return View("Register");
+            }
+
+            if(userDto.Username == null || userDto.Email == null || userDto.Password == null)
+            {
+                ViewBag.Message = "Please check whether all the required fields (Username, Email and Password) are filled!";
+                return View("Register");
+            }
+
+            var doesUserExist = await context.Users.FirstOrDefaultAsync(User => User.Email == userDto.Email);
+
+            if(doesUserExist == null)
+            {
+                var user = new User
+                {
+                    Username = userDto.Username,
+                    Email = userDto.Email,
+                    Password = userDto.Password,
+                };
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "User already exits. Please Login!";
+                return View("Register");
+            }
         }
     }
 }
---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductDesk.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
